77b9e4e25ccfbee422a543f0768b1eedf88c6921
Gaurav Harchwani <gaurav@getzave.com>
2020-04-07 18:22:09 +0800

    added more granularaity on searching result message + refactored API query to use config flags and not hardcoded values

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -5,7 +5,7 @@ const useResources = (resource) => {
     const [movies, setMovies] = useState([]);
     
     const fetchResource = async (resource) =>  {
-        const query = `3/search/movie?api_key=${movieDB.apiKey}&language=en-US&page=1&include_adult=false&query=${resource}`
+        const query = `3/search/movie?api_key=${movieDB.apiKey}&page=1&include_adult=${movieDB.showAdult}&query=${resource}`
         if(resource) {
             const response = await fetch(movieDB.url+`${query}`);
             let data = await response.json();
@@ -24,11 +24,31 @@ const useResources = (resource) => {
 
 const MovieList = ({ search }) => {
     const resources = useResources(search);
-    return (
-        <ul>
-            { resources.map((record) => <li key={record.id}>{record.title}</li>)} 
-        </ul>
-    )
+    if(resources.length>1) {
+        return (
+            <ul>
+                {resources.map((record) => 
+                    <li onClick={(e) => console.log(e.target.dataset['item'])} key={record.id} data-item={record.id}>
+                        {record.title}
+                    </li>
+                )} 
+            </ul>
+        )
+    } else {
+        if(search.length === 0) {
+            return (
+                <div>
+                    Begin your search now.
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    No results found.
+                </div>
+            )
+        }
+    }
 }
 
 export default MovieList;
--- src/configs/config.js
+++ src/configs/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     "movieDB": {
         "url": "https://api.themoviedb.org/",
-        "apiKey": "87ff86bf3d3fb3d192ad02945864b277"
+        "apiKey": "87ff86bf3d3fb3d192ad02945864b277",
+        "showAdult": false 
     }
 }
