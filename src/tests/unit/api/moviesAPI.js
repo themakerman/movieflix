252f54fe583639f79e87b7d9fb9b910280b300ea
Gaurav Harchwani <gaurav@getzave.com>
2020-04-08 15:04:47 +0800

    added unit test for testing movieDB api endpoints

--- src/tests/unit/api/moviesAPI.js
+++ src/tests/unit/api/moviesAPI.js
@@ -1,35 +1,46 @@
-const movieDB = require('../../../configs/config');
-const fetch = require('node-fetch');
+const { movieDB } = require('../../../configs/config');
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
 const assert = require('chai').assert;
 
 
 //Mocha test function
-describe.only('Movies API unit testing', async function () {
+describe.only('Movies API unit testing', function () {
     
     const MOVIE_LIST_QUERY = `${movieDB.url}3/search/movie?api_key=${movieDB.apiKey}&page=1&include_adult=${movieDB.showAdult}&query=a`; 
-    const MOVIE_DETAIL_QUERY = `${movieDB.url}3/movie/513573`;
+    const MOVIE_DETAIL_QUERY = `${movieDB.url}3/movie/513573?api_key=${movieDB.apiKey}`;
     
     before((done) => { 
         done();
     })
     
-    it('Able to reach movies search api', async (done) => {
-        const response = await fetch(MOVIE_LIST_QUERY);
-        if(response.ok) {
-            done()
-        } else {
-            done(new Error('Unable to reach the movies search api'))
-        }
+    it('Able to reach movies search api', (done) => {
+        fetch(MOVIE_LIST_QUERY)
+        .then((data) => {
+            if(data.status !== 200) {
+                done(new Error('Unable to reach movies search api'))
+            } else {
+                done();
+            }
+        })
+        .catch((error) => {
+          done(error)
+        });
         
     });
     
-    it('Able to reach movies details api', async (done) => {
-        const response = await fetch(MOVIE_DETAIL_QUERY);
-        if(response.ok) {
-            done()
-        } else {
-            done(new Error('Unable to reach the movie details api'))
-        }
+    it('Able to reach movies details api', (done) => {
+        fetch(MOVIE_DETAIL_QUERY)
+        .then((data) => {
+            if(data.status !== 200) {
+                done(new Error('Unable to reach movies search api'))
+            } else {
+                done();
+            }
+        })
+        .catch((error) => {
+          done(error)
+        });
     });
             
     after((done) => {
